f6dbdc84ec9db7652da2d5009136bcf4613fc178
msciborski <michal.sciborski@student.put.poznan.pl>
2019-03-12 20:00:03 +0100

    authentication reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { profileMenu } from "./profileMenu.reducer";
 import { registration } from "./registration.reducer";
+import { authentication } from "./authentication.reducer";
 
 const rootReducer = combineReducers({
   profileMenu,
   registration,
+  authentication,
 });
 
 export default rootReducer;
