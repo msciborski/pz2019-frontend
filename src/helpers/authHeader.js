8bfa7e79f0ed49b755563d9d0055f8dd7a1e93ec
msciborski <michal.sciborski@student.put.poznan.pl>
2019-03-12 19:44:33 +0100

    AuthHeader

---
+++ src/helpers/authHeader.js
@@ -0,0 +1,9 @@
+export const authHeader = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user && user.token) {
+    return { 'X-AUTH-TOKEN': user.token };
+  } else {
+    return {};
+  }
+}
