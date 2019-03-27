47f5af5e087fa2d753180995d89f5f805572ad9f
Jakub <kubasmieszny@gmail.com>
2019-03-27 17:57:59 +0100

    change password

--- src/_components/ChangePasswordDialog/ChangePasswordDialog.jsx
+++ src/_components/ChangePasswordDialog/ChangePasswordDialog.jsx
@@ -10,21 +10,114 @@ class ChangePasswordDialog extends Component {
             newPassword: '',
         }
     }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value,
+        });
+    }
+    handleSubmit = () => {
+        const { oldPassword, newPassword } = this.state;
+        const { userId, changePassword } = this.props;
+
+        changePassword(oldPassword, newPassword, userId);
+    }
+
+    render() {
+        const { open, handleClose, user } = this.props;
+        const { userToUpdate } = this.state;
+
+        return (
+        <Dialog
+            open={open}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={handleClose}
+            aria-labelledby="change-password"
+            aria-describedby="change-password-description"
+        >
+            <ValidatorForm onSubmit={this.handleSubmit}>
+            <DialogTitle id="edit-basic-info-title">
+                {"Edit"}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                {`Update user: ${user.name} ${user.surname}`}
+                </DialogContentText>
+                <InputWithLabel
+                label="Voivodeship"
+                onChange={this.handleEditChange}
+                value={userToUpdate.voivodeship}
+                name="voivodeship"
+                margin="normal"
+                autoComplete="voivodeship"
+                isFullWidth={true}
+                />
+                <InputWithLabel
+                label="Street"
+                onChange={this.handleEditChange}
+                value={userToUpdate.street}
+                name="street"
+                margin="normal"
+                autoComplete="street"
+                isFullWidth={true}
+                />
+                <InputWithLabel
+                label="City"
+                onChange={this.handleEditChange}
+                value={userToUpdate.city}
+                name="city"
+                margin="normal"
+                autoComplete="city"
+                isFullWidth={true}
+                />
+                <InputWithLabel
+                label="Number"
+                onChange={this.handleEditChange}
+                value={userToUpdate.number}
+                name="number"
+                margin="normal"
+                autoComplete="number"
+                isFullWidth={true}
+                />
+                <InputWithLabel
+                label="Zip Code"
+                onChange={this.handleEditChange}
+                value={userToUpdate.zipCode}
+                name="zipCode"
+                margin="normal"
+                autoComplete="zipCode"
+                isFullWidth={true}
+                />
+                <InputWithLabel
+                label="Phone"
+                onChange={this.handleEditChange}
+                value={userToUpdate.phone}
+                name="phone"
+                margin="normal"
+                autoComplete="phone"
+                isFullWidth={true}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button color="primary" onClick={handleClose}>
+                Cancel
+                </Button>
+                <Button
+                color="primary"
+                type="submit"
+                >
+                Save
+            </Button>
+            </DialogActions>
+            </ValidatorForm>
+        </Dialog>
+        )
+    }
 }
 
-handleChange = event => {
-    const { name, value } = event.target;
-
-    this.setState({
-        [name]: value,
-    });
-  }
-handleSubmit = () => {
-    const { oldPassword, newPassword } = this.state;
-    const { userId, changePassword } = this.props;
-
-    changePassword(oldPassword, newPassword, userId);
-}
 
 const mapStateToProps = state => {
     const { authentication } = state;
