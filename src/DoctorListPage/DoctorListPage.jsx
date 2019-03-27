08f22081280a66c65be69fc722de40de6bbe25e0
msciborski <michal.sciborski@student.put.poznan.pl>
2019-03-27 20:15:41 +0100

    Making table in doctor list page

--- src/DoctorListPage/DoctorListPage.jsx
+++ src/DoctorListPage/DoctorListPage.jsx
@@ -2,30 +2,47 @@ import React from "react";
 import { Component } from "react";
 import { connect } from "react-redux";
 import { doctorsActions } from "../_actions";
-import { Grid, Typography, Paper } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
+import { Grid, Typography, Paper, Table, TableHead, TableCell, TableBody, TableRow } from "@material-ui/core";
+
+const styles = {
+  root: {
+    maxWidth: '1500px !important',
+    margin: '20px 0 20px 0 !important',
+  },
+};
 
 class DoctorListPage extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     componentDidMount() {
         const { getDoctors } = this.props;
         getDoctors();
     }
 
     render() {
-        const { doctors } = this.props;
+        const { doctors, classes } = this.props;
         console.log('Doctors', doctors);
 
         return (
-            <Grid item xs={12}>
+            <Grid item xs={12} className={classes.root}>
                 {
                     doctors &&
                         <div>
                             <Typography variant="h3">Doctors</Typography>
                             <Paper>
-                                    
+                              <Table>
+                                <TableHead>
+                                  <TableCell>Doctor</TableCell>
+                                  <TableCell>Specializations</TableCell>
+                                </TableHead>
+                                <TableBody>
+                                  {doctors.map(doctor => (
+                                    <TableRow id={doctor.id}>
+                                      <TableCell>{`${doctor.name} ${doctor.surname}`}</TableCell>
+                                      <TableCell>{doctor.specializations.join(',')}</TableCell>
+                                    </TableRow>
+                                  ))}
+                                </TableBody>
+                              </Table>
                             </Paper>
                         </div>
                 }
@@ -49,4 +66,5 @@ const mapDispatchToProps = dispatch => {
 }
 
 const connectedDoctorListPage = connect(mapStateToProps, mapDispatchToProps)(DoctorListPage);
-export { connectedDoctorListPage as DoctorListPage };
+const styledConnectedDoctorListPage = withStyles(styles)(connectedDoctorListPage);
+export { styledConnectedDoctorListPage as DoctorListPage };
